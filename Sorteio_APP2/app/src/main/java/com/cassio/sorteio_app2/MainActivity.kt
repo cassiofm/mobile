812f036dcdd1106c687158b4f06dc02fb8afdd9c
Cassio <cassiofm@bol.com.br>
2021-07-04 20:53:43 -0300

    commit do app

--- Sorteio_APP2/app/src/main/java/com/cassio/sorteio_app2/MainActivity.kt
+++ Sorteio_APP2/app/src/main/java/com/cassio/sorteio_app2/MainActivity.kt
@@ -2,10 +2,21 @@ package com.cassio.sorteio_app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun SortearNumero(view: View){
+        var texto = findViewById(R.id.txtNumero) as TextView
+
+        var numeroSorteado = Random().nextInt(11)
+
+        texto.setText("O número é: $numeroSorteado")
     }
 }
