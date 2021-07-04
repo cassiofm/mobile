68eb6e16c0c3adcbafa2ff79d1543b3ad8550689
Cassio <cassiofm@bol.com.br>
2021-07-04 20:30:50 -0300

    Initial commit

---
+++ OlaMundo_APP1/app/src/main/java/com/cassio/olamundo_app1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cassio.olamundo_app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
