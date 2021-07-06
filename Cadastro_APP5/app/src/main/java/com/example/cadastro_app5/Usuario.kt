f0975d5abdc8e7c16d26c126cafa32b600f311fc
Cassio <cassiofm@bol.com.br>
2021-07-06 13:00:47 -0300

    app 5 adicionado

---
+++ Cadastro_APP5/app/src/main/java/com/example/cadastro_app5/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.cadastro_app5
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Usuario(val nome: String, val idade: Int) : Parcelable
